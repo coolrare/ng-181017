c2b6d2b0eda87c023d946eea336fe112fa1bda3d
Will <doggy.huang@gmail.com>
2018-10-17 19:19:24 +0800

    修正 Layout 的 Initialize JS

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutInit } from './LayoutInit';
 
 @Component({
   selector: 'app-layout',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    LayoutInit();
   }
-
 }
