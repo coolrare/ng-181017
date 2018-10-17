a449b87344c9b34ae0dc0c356aefc53c56060f6f
Will <doggy.huang@gmail.com>
2018-10-17 14:43:44 +0800

    完成 dashboard 與 forms 頁面套版 (SPA)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsComponent } from './forms/forms.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'page1', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'forms', component: FormsComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
