63c01d7ae48509d868152ba4567723608f7a59d9
Will <doggy.huang@gmail.com>
2018-10-18 15:46:55 +0800

    初始化 FormGroup 表單模型

--- src/app/forms2/forms2.component.ts
+++ src/app/forms2/forms2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-forms2',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Forms2Component implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      name: 'Will',
+      tel: '0944-444444'
+    })
   }
 
 }
